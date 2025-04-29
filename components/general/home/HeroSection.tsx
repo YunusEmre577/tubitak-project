b593d01fa9981345d4f166c3358210f7c56583f6
YunusEmre577 <yunusemres0001@gmail.com>
2025-04-29 14:59:04 +0300

    Update HeroSection.tsx
    New button

--- components/general/home/HeroSection.tsx
+++ components/general/home/HeroSection.tsx
@@ -9,10 +9,13 @@ export default function HeroSection() {
     <div className="flex flex-col items-center gap-6">
     <h1 className="text-5xl lg:text-6xl text-center max-w-md lg:max-w-xl font-black">Barajlarımız buhar olup <span className='text-colored'>uçmasın</span></h1>
     <p className="text-muted text-lg max-w-md lg:max-w-lg text-center">Baraj göllerinin buharlaşma oranını düşürmek için yüzer güneş panelleri sistemi kurarak buharlaşmayı azaltmak ve tarım arazilerine ayrılmış alanların işgal edilmesini önlemek için buradayız.</p>
-    <div className="flex flex-col w-full lg:justify-center lg:items-center responsive">
+    <div className="flex flex-col w-full lg:justify-center lg:items-center gap-2 responsive">
     <Link className='w-full lg:w-fit flex flex-col' href={"/project"}>
     <Button className='w-full lg:w-fit' icon={"solar:alt-arrow-right-outline"}>Projeye git</Button>
     </Link>
+<Link className="w-full lg:w-fit" href="#ekip">
+<Button icon="solar:arrow-down">Aşağı in</Button>
+</Link>
     </div>
     </div>
   </section>
