f5edb8f189b26ebd027411eb77ffb0cc931d0cd0
YunusEmre577 <yunusemres0001@gmail.com>
2025-04-30 11:53:11 +0300

    Update HeroSection.tsx

--- components/general/home/HeroSection.tsx
+++ components/general/home/HeroSection.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function HeroSection() {
   return (
-    <section className="w-full flex flex-col pt-24 lg:pt-32 items-center">
+    <section className="w-full responsive flex flex-col pt-24 lg:pt-32 items-center">
     <div className="flex flex-col items-center gap-6">
     <h1 className="text-5xl lg:text-6xl text-center max-w-md lg:max-w-xl font-black">Barajlarımız buhar olup <span className='text-colored'>uçmasın</span></h1>
     <p className="text-muted text-lg max-w-md lg:max-w-lg text-center">Baraj göllerinin buharlaşma oranını düşürmek için yüzer güneş panelleri sistemi kurarak buharlaşmayı azaltmak ve tarım arazilerine ayrılmış alanların işgal edilmesini önlemek için buradayız.</p>
